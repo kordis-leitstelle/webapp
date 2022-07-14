75e15c1b76b72b451d58dd1d718ee96a4d3df64e
Timon Masberg <contact@timonmasberg.com>
2022-07-15 01:24:50 +0200

    fix(linting): change unsafe call and member access eslint rule to warn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,42 +1,34 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	extends: [
-		'eslint:recommended',
-		'plugin:import/recommended',
-		'plugin:import/typescript',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking'
-	],
-	rules: {
-		"import/no-unresolved": "error",
-		// svelte currently does not support markdown typescript, but sometimes we want to use inline functions (events etc.)
-		"@typescript-eslint/no-unsafe-call": "warn",
-		"@typescript-eslint/no-unsafe-member-access": "warn",
-	},
-	parserOptions: {
-		tsconfigRootDir: __dirname,
-		project: ['./tsconfig.json'],
-		extraFileExtensions: ['.svelte'],
-	},
-	overrides: [
-		{
-			files: ['*.svelte'],
-			processor: 'svelte3/svelte3'
-		}
-	],
-	settings: {
-		'svelte3/typescript': require('typescript'),
-		'svelte3/ignore-styles': () => true,
-		"import/resolver": {
-			"typescript": {
-				"alwaysTryTypes": true,
-			}
-		}
-	},
-	plugins: ['import', 'svelte3', '@typescript-eslint'],
-	ignorePatterns: ['node_modules'],
-	globals: {
-		"__kordisConfig": "readonly",
-		"__appDetails": "readonly"
-	}
-}
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:import/recommended', 'plugin:import/typescript', 'plugin:@typescript-eslint/recommended', 'plugin:@typescript-eslint/recommended-requiring-type-checking', 'plugin:storybook/recommended'],
+  rules: {
+    "import/no-unresolved": "error",
+    // svelte currently does not support markdown typescript, but sometimes we want to use inline functions (events etc.)
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn"
+  },
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    extraFileExtensions: ['.svelte']
+  },
+  overrides: [{
+    files: ['*.svelte'],
+    processor: 'svelte3/svelte3'
+  }],
+  settings: {
+    'svelte3/typescript': require('typescript'),
+    'svelte3/ignore-styles': () => true,
+    "import/resolver": {
+      "typescript": {
+        "alwaysTryTypes": true
+      }
+    }
+  },
+  plugins: ['import', 'svelte3', '@typescript-eslint'],
+  ignorePatterns: ['node_modules'],
+  globals: {
+    "__kordisConfig": "readonly",
+    "__appDetails": "readonly"
+  }
+};
