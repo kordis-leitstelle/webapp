0783e3313207ed13bffe5fe45b707f179e0379cc
Timon Masberg <contact@timonmasberg.com>
2022-07-16 12:59:01 +0200

    feat: add loading button component

---
+++ src/lib/components/common/LoadingButton/LoadingButton.stories.svelte
@@ -0,0 +1,20 @@
+<script>
+	import {Meta, Story, Template} from '@storybook/addon-svelte-csf';
+	import LoadingButton from './LoadingButton.svelte';
+
+	const buttonClass =
+		'krd-button bg-button-primary hover:bg-button-primary-hover text-white';
+</script>
+
+<Meta title="Loading Button" component={LoadingButton} />
+
+<Template let:args>
+	<LoadingButton {...args}>Slot content</LoadingButton>
+</Template>
+<Template let:args>
+	<LoadingButton {...args} />
+</Template>
+
+<Story name="Loading" args={{isLoading: true, buttonClass}} />
+<Story name="Not loading (Default)" args={{isLoading: false, buttonClass}} />
+<Story name="Default" />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,23 @@ const forms = require("@tailwindcss/forms");
 
 const config = {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+				"input-focus": "rgb(14, 165, 233)", // blue 500
+				"input": "rgb(209 213 219)", // gray 300
+				"input-error": "rgb(245, 101, 101)", // red 500
+				"label-error": "rgb(220 38 38)", // red 600
+				"input-error-focus": "rgb(220 38 38)", // red 600
+				"icon-hover": "rgb(14, 165, 233)", // blue 500
+				"button-primary": "rgb(37, 99, 235)", // blue 600
+				"button-primary-hover": "rgb(29, 78, 216)", // blue 700
+			},
+			screens: {
+				"3xl": "2080px"
+			}
+		},
   },
-
   plugins: [forms],
 };
 
